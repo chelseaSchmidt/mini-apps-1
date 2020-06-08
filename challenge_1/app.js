0c260b839278bb2fc5633b362f4e056ea4b95bb4
Chelsea Schmidt <cschmidt331@gmail.com>
2020-06-08 10:59:48 -0700

    Added visual turn tracker element, connected app.js, planned approach

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,20 @@
+//CONTROLLER
+  //event listeners for clicks on cells
+    //on click, place an X or O in target cell in VIEW and MODEL
+      //check for win or tie
+      //change whose turn it is
 
+  //event listener for reset button click
+    //on click, empty all cells in VIEW and MODEL
 
+//MODEL
+  //variable to track whose turn it is
+  //variable to track board matrix
+  //function to check for a win or tie
+  //function to reset matrix to empty
+
+//VIEW
+  //function to render X's and O's to page
+  //function to change whose turn is displayed
+  //function to reset board
+  //function to display message upon win or tie
