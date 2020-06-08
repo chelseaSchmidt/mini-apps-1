a1c7a473a6e907b5482161da299677ef2b1a4508
Chelsea Schmidt <cschmidt331@gmail.com>
2020-06-08 11:17:06 -0700

    Added event listeners for each board cell

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,20 +1,51 @@
 //CONTROLLER
-  //event listeners for clicks on cells
-    //on click, place an X or O in target cell in VIEW and MODEL
-      //check for win or tie
-      //change whose turn it is
+//event listeners for clicks on cells
+let cellElements = document.getElementsByClassName('board-cell');
+cellElements = [...cellElements];
+cellElements.forEach((cell) => {
+  cell.addEventListener('click', (event) => {
+    console.dir(event.target);
+  });
+});
+  //on click, place an X or O in target cell in VIEW and MODEL
+    //check for win or tie
+    //change whose turn it is
 
-  //event listener for reset button click
-    //on click, empty all cells in VIEW and MODEL
+//event listener for reset button click
+  //on click, empty all cells in VIEW and MODEL
 
 //MODEL
-  //variable to track whose turn it is
-  //variable to track board matrix
-  //function to check for a win or tie
-  //function to reset matrix to empty
+const model = {
+  player: 'X',
+  board: [
+    ['','',''],
+    ['','',''],
+    ['','',''],
+  ],
+  checkForWinOrTie: () => {
+
+  },
+  resetModel: () => {
+
+  },
+  changeTurn: () => {
+
+  }
+};
+
 
 //VIEW
-  //function to render X's and O's to page
-  //function to change whose turn is displayed
-  //function to reset board
-  //function to display message upon win or tie
+const view = {
+  renderClick: (player) => {
+
+  },
+  changeDisplayedTurn: () => {
+
+  },
+  resetBoard: () => {
+
+  },
+  showWinOrTie: () => {
+
+  }
+};
