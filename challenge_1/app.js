66834c80ab4fae888f23e6a2cfacba235b961d1c
Chelsea Schmidt <cschmidt331@gmail.com>
2020-06-08 11:51:24 -0700

    Added ability to affect modeled board matrix on click

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -4,8 +4,7 @@ let cellElements = document.getElementsByClassName('board-cell');
 cellElements = [...cellElements];
 cellElements.forEach((cell) => {
   cell.addEventListener('click', (event) => {
-    view.renderClick(model.player, event.target);
-    model.changeTurn();
+    model.placeMarker(model.player, event.target);
   });
 });
   //on click, place an X or O in target cell in VIEW and MODEL
@@ -23,6 +22,22 @@ const model = {
     ['','',''],
     ['','',''],
   ],
+  rows: {
+    A: 0,
+    B: 1,
+    C: 2
+  },
+  columns: {
+    1: 0,
+    2: 1,
+    3: 2
+  },
+  placeMarker: (player, targetCell) => {
+    const row = model.rows[targetCell.id[0]];
+    const column = model.columns[targetCell.id[1]];
+    model.board[row][column] = player;
+    console.log(model.board);
+  },
   checkForWinOrTie: () => {
 
   },
@@ -32,8 +47,10 @@ const model = {
   changeTurn: () => {
     if (model.player === 'X') {
       model.player = 'O';
+      view.changeDisplayedTurn('O');
     } else {
       model.player = 'X';
+      view.changeDisplayedTurn('X');
     }
   }
 };
@@ -45,7 +62,7 @@ const view = {
     console.log('click!');
     eventTarget.append('X');
   },
-  changeDisplayedTurn: () => {
+  changeDisplayedTurn: (newTurn) => {
 
   },
   resetBoard: () => {
