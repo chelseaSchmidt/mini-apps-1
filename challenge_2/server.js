c4e7a31093fa068ec158dfc8cd9da3f8ffa45f37
Chelsea Schmidt <cschmidt331@gmail.com>
2020-06-09 11:07:03 -0700

    Wrote express server and reorganized file layout

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+//middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+//static files
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+//routes
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+//listen
+app.listen(port, () => {
+  console.log('server is listening');
+});
