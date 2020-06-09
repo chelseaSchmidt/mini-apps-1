1d919975af71bee21f6f2351739666946bef73ce
Chelsea Schmidt <cschmidt331@gmail.com>
2020-06-09 12:49:25 -0700

    Fixed form input and post route to expect/parse JSON or send back 400

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -8,21 +8,43 @@ const path = require('path');
 
 //middleware
 app.use(morgan('dev'));
-app.use(bodyParser.json());
 
 //static files
 app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(bodyParser.text());
 
 //routes
-app.get('/', (req, res) => {
-  res.sendStatus(200);
-});
 
 app.post('/', (req, res) => {
+
   //do stuff with JSON data in req.data
   //return result in response
-  res.status(418);
-  res.send('still working on this');
+  //also return form in response
+  let dataIndex = req.body.indexOf('=') + 1;
+  let formString = req.body.slice(dataIndex);
+  try {
+    JSON.parse(formString);
+    res.status(418);
+    res.send('still working on this');
+  } catch (error) {
+    console.log('nope');
+    res.status(400);
+    res.send('not JSON format');
+  }
+
+
+  // req.on('data', chunk => {
+  //   formString += chunk.toString();
+  // });
+
+  // req.on('end', () => {
+  //   let dataIndex = formString.indexOf('=') + 1;
+  //   formString = formString.slice(dataIndex);
+  //   // console.log(formString);
+  //   res.status(418);
+  //   res.send('still working on this');
+  // });
+
 });
 
 //listen
